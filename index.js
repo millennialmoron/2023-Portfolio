3053cac9df8c0d4fb786fda6f05b7faf8071c2a2
millennialmoron <millennialmoron87@gmail.com>
2023-07-12 17:02:34 -0500

    finished updates to site

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".from-above");
+const border1 = document.querySelectorAll(".left-to-right");
+const border2 = document.querySelectorAll(".right-to-left");
+const border3 = document.querySelectorAll(".border-center");
+const border4 = document.querySelectorAll(".border-back-center");
 
 const appearOptions = {
   threshold: 0,
@@ -26,4 +30,20 @@ faders.forEach((fader) => {
 
 sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
+});
+
+border1.forEach((border) => {
+  appearOnScroll.observe(border);
+});
+
+border2.forEach((border) => {
+  appearOnScroll.observe(border);
+});
+
+border3.forEach((border) => {
+  appearOnScroll.observe(border);
+});
+
+border4.forEach((border) => {
+  appearOnScroll.observe(border);
 });
